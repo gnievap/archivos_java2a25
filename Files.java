0e5b79475b151dc9f51f5dc746bf64c337369397
Gaby Nieva <gaby.nieva@outlook.com>
2025-04-02 10:02:48 -0600

    Inicializando objetos y variables

--- Files.java
+++ Files.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Files{
-    
+
+    public int[] fileToIntArray(String fileName){
+        File file; // Apuntar a un archivo físico del dd
+        FileReader reader; //Llave con permiso de sólo lectura
+        BufferedReader bufer; // Para recuperar información del archivo
+        String linea; // Una línea del archivo
+        int[] array = null; // arreglo de números resultante
+        int i=0; // indice
+        int t; //tamaño del arreglo
+        try{
+            t = countFileLines(fileName);
+            array = new int[t];
+            file = new File("C:\\archivos\\" + fileName);
+            reader = new FileReader(file);
+            bufer = new BufferedReader(reader);
+        } catch( Exception e){
+            System.out.println("Error al leer el archivo: " + e.toString());
+        }
+
+    }
+
 }
